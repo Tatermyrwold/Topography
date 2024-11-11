439b9389fda729ee573188b11aba9457d06a9580
Tatermyrwold <Tatermyrwold@gmail.com>
2024-11-11 13:19:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, requests
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
